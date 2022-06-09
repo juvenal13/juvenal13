d007bc19fa2245a33bc6fedc0a3e43f163db4474
juvenal ATTOUMBRE <jattoumbre@sensingvision.com>
2022-06-09 15:15:01 +0200

    add models

---
+++ models/organisationsModel.js
@@ -0,0 +1,18 @@
+class OrganisationsModel extends Model {
+  constructor() {
+    super();
+  }
+
+  id() {
+    return this.id;
+  }
+  name() {
+    return this.name;
+  }
+  id(_id) {
+    this.id = _id;
+  }
+  name(_name) {
+    this.name = _name;
+  }
+}
---
+++ models/sensorsModel.js
@@ -0,0 +1,31 @@
+class SensorsModel extends Model {
+  constructor() {
+    super();
+  }
+
+  id() {
+    return this.id;
+  }
+  name() {
+    return this.name;
+  }
+  organisationId() {
+    return this.organisationId;
+  }
+  sensortypeId() {
+    return this.sensorTypeId;
+  }
+
+  id(_id) {
+    this.id = _id;
+  }
+  name(_name) {
+    this.name = _name;
+  }
+  organisationId(_organisationId) {
+    this.organisationId = _organisationId;
+  }
+  sensortypeId(_sensorTypeId) {
+    this.sensorTypeId = _sensorTypeId;
+  }
+}
