94874bdfa1eec8aeb4e0355cb2209b097167f258
juvenal ATTOUMBRE <jattoumbre@sensingvision.com>
2022-06-10 11:24:49 +0200

    add update and delete in routes and controllers

--- controllers/sensorsController.js
+++ controllers/sensorsController.js
@@ -22,8 +22,51 @@ const createsensor = (req, res) => {
   const createSensor = sensorService.createNewSensor(newSensor);
   res.status(201).send({ status: "OK", data: createSensor });
 };
+
+const updateSensor = (req, res) => {
+  const {
+    body,
+    params: { sensorId },
+  } = req;
+  if (!sensorId) {
+    res.status(400).send({
+      status: "FAILED",
+      data: { error: "Parameter ':sensorId' can not be empty" },
+    });
+  }
+  try {
+    const updatedSensor = sensorService.updateOneSensor(sensorId, body);
+    res.status(201).send({ status: "OK", data: updatedSensor });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
+};
+const deleteSensor = (req, res) => {
+  const {
+    params: { sensorId },
+  } = req;
+  if (!sensorId) {
+    res.status(400).send({
+      status: "FAILED",
+      data: { error: "Parameter ':workoutId' can not be empty" },
+    });
+  }
+  try {
+    sensorService.deleteOneSensor(sensorId);
+    res.status(204).send({ status: "OK" });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
+};
+
 module.exports = {
   getAllSensors,
   getSensorsByOrganisationId,
   createsensor,
+  updateSensor,
+  deleteSensor,
 };
--- routes/sensorsRoute.js
+++ routes/sensorsRoute.js
@@ -6,4 +6,8 @@ router.get("/", sensorController.getAllSensors);
 router.get("/:organisationId", sensorController.getSensorsByOrganisationId);
 
 router.post("/", sensorController.createsensor);
+
+router.delete("/delete/:sensorId", sensorController.deleteSensor);
+
+router.put("/update/:sensorId", sensorController.updateSensor);
 module.exports = router;
